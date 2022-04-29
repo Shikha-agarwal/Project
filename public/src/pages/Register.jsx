4748e78cd08359e411eaaefbbbf8f06eda219f15
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2022-04-29 22:20:23 +0530

    commit

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -2,56 +2,102 @@ import React,{useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import Logo from "../assets/logo.svg";
+import {ToastContainer,toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from '../utils/APIRoutes';
 
 function Register() {
     const [values,setValues] = useState({
         username: "",
         email: "",
-    })
-    const handleSubmit = (event)=> {
+        password: "",
+        confirmPassword: "",
+    });
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: 'dark',
+    }
+    const handleSubmit = async (event)=> {
         event.preventDefault();
-        alert("form");
+        if(handleValidation()) {
+            const {password,confirmPassword,username,email} = values;
+            const {data} = await axios.post(registerRoute,{
+                username,
+                email,
+                password,
+            });
+        }
     };
-    const handleChange = (event)=> {};
+
+    const handleValidation = ()=> {
+        const {password,confirmPassword,username,email} = values;
+        if(password!==confirmPassword) {
+            console.log("inm validation", toast);
+            toast.error(
+            "Password and Confirm Password should be same.", 
+            toastOptions
+            );
+            return false;
+        } else if (username.length<=3) {
+            toast.error("Username should be greater than 3 characters.", toastOptions);
+            return false;
+        } else if (password.length<8) {
+            toast.error("Password should be equal or greater than 8 characters.", toastOptions);
+            return false;
+        } else if(email === "") {
+            toast.error("email is required", toastOptions);
+            return false;
+        }
+        return true;
+    };
+
+    const handleChange = (event)=> {
+        setValues({...values,[event.target.name]:event.target.value});
+    };
     return (
-            <FormContainer>
-                <form onSubmit={(event)=>handleSubmit(event)}>
-                    <div className="brand">
-                        <img src={Logo} alt="Logo"></img>
-                        <h1>snappy</h1>
-                    </div>
-                    <input 
-                        type="text" 
-                        placeholder="Username" 
-                        name="username" 
-                        onChange={(e) => handleChange(e)} 
-                    />
-                     <input 
-                        type="email" 
-                        placeholder="Email" 
-                        name="email" 
-                        onChange={(e) => handleChange(e)} 
-                    />
-                     <input 
-                        type="password" 
-                        placeholder="Password" 
-                        name="password" 
-                        onChange={(e) => handleChange(e)} 
-                    />
-                     <input 
-                        type="password" 
-                        placeholder="Confirm Password" 
-                        name="confirmPassword" 
-                        onChange={(e) => handleChange(e)} 
-                    />
-                    <button type="submit">Create User</button>
-                    <span>
-                        Already have an account ? <Link to="/login">Login</Link>
-                    </span>
-                </form>
+          <FormContainer>
+            <form action="" onSubmit={(event) => handleSubmit(event)}>
+              <div className="brand">
+                <img src={Logo} alt="logo" />
+                <h1>snappy</h1>
+              </div>
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={(e) => handleChange(e)}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={(e) => handleChange(e)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => handleChange(e)}
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                onChange={(e) => handleChange(e)}
+              />
+              <button type="submit">Create User</button>
+              <span>
+                Already have an account ? <Link to="/login">Login.</Link>
+              </span>
+            </form>
+            <ToastContainer />
             </FormContainer>
-    );
-}
+        );
+    }
 
 const FormContainer = styled.div`
     height: 100vh;
